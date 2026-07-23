c88d0f27cfd6b81b94c2ed2c2af92de495b5c48b
Divyadharshini R <divyadharshinirdd03@gmail.com>
2026-07-23 11:15:53 +0530

    Time: 2 ms (82.40%) | Memory: 46.4 MB (82.38%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> results = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) Arrays.fill(row, '.');
+        backtrack(0, board, results, n);
+        return results;
+    }
+    private void backtrack(int row, char[][] board, List<List<String>> results, int n) {
+        if (row == n) {
+            results.add(construct(board));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isSafe(board, row, col, n)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1, board, results, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q') return false;
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q') return false;
+
+        return true;
+    }
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (char[] row : board) res.add(new String(row));
+        return res;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solveNQueens(4));
+    }
+}
